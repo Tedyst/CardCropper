e2784fa51692491f2802df3b8cf589624cfe45ee
Stoica Tedy <stoicatedy@gmail.com>
2020-08-04 17:38:26 +0300

    asd

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,19 @@ import DropZone from './components/DropZone'
 import Card from './components/Card';
 import BrowserImageManipulation from 'browser-image-manipulation'
 import Saver from './components/Saver';
+import ImageSize from 'image-size';
 
 function App() {
   const [image, setImage] = React.useState<ArrayBuffer>();
   const [result, setResult] = React.useState<string[]>();
+  const [settings, setSettings] = React.useState({
+    x: 100,
+    y: 100,
+    number: 10,
+  })
 
   let arr: JSX.Element[] = [];
   if (result) {
-    console.log(result);
     for (let val in result) {
       arr.push(<Card image={result[val]} key={"card" + val} />)
     }
@@ -21,12 +26,19 @@ function App() {
       let byteArray = new Uint8Array(image);
       let blob = new Blob([byteArray], { type: 'image/png' });
 
+      let i = new Image();
+      i.src = objectURL;
+      i.onload = function () {
+        alert(i.width + ", " + i.height);
+      };
+
+
       let promises = [];
       // Start all promises
-      for (let i = 0; i < 10; i++) {
+      for (let i = 0; i < settings.number; i++) {
         promises.push(new BrowserImageManipulation()
           .loadBlob(blob)
-          .crop(100, 100, 0.0001 + i * 100, 0.0001 + 0)
+          .crop(settings.x, settings.y, 0.0001 + i * 100, 0.0001 + 0)
           .saveAsImage());
       }
       // Wait for all promises to finish
