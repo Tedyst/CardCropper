30dcbcc1a21e7e90013c67be7e2209177e22d422
Stoica Tedy <stoicatedy@gmail.com>
2020-08-05 13:46:05 +0300

    added settings, they dont do anything rn

---
+++ src/components/Settings.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Settings(props: {
+    settings: {
+        x: number;
+        y: number;
+        number: number;
+    },
+    setSettings: React.Dispatch<React.SetStateAction<{
+        x: number;
+        y: number;
+        number: number;
+    }>>
+}) {
+
+    return null;
+}
