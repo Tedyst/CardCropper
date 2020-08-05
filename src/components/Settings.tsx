ccfce6d88a3665914937dcdf4bfc1433cec758ce
Stoica Tedy <stoicatedy@gmail.com>
2020-08-05 13:55:25 +0300

    settings change now regens images

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { MouseEvent } from 'react'
 
 export default function Settings(props: {
     settings: {
@@ -10,8 +10,16 @@ export default function Settings(props: {
         x: number;
         y: number;
         number: number;
-    }>>
+    }>>,
+    setResult: React.Dispatch<React.SetStateAction<string[]>>
 }) {
-
-    return null;
+    function handleClick(e: MouseEvent) {
+        props.setSettings({
+            number: 5,
+            x: 300,
+            y: 300
+        })
+        props.setResult([]);
+    }
+    return <a href="/#" onClick={handleClick}>Change Settings</a>;
 }
