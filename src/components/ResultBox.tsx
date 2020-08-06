693ee7f35a0ef34cb8046a73d1061e6fe5b35806
Stoica Tedy <stoicatedy@gmail.com>
2020-08-06 12:04:02 +0300

    nice

--- src/components/ResultBox.tsx
+++ src/components/ResultBox.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Card, CardContent, Grid, CardMedia } from '@material-ui/core';
+import { Card, Grid, CardMedia } from '@material-ui/core';
 
 export default function ResultBox(props: {
     images: string[],
